bfaae1590f37d095b135fced859142df3a14cb01
mahiyou <you.maede@gmail.com>
2023-08-26 12:49:51 +0330

    Footer became responsive.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,15 +8,15 @@ function Footer() {
     return (
         <div className='footer-style'>
             <Row className="bg-primary mt-4 py-3 mx-0">
-                <Col className="text-center my-auto" xs={2}>
+                <Col className="text-center my-auto hidden-md-down" xs={12} md={2}>
                     <FontAwesomeIcon icon={faCircle} size="2xs"/>
                     <a className="text-white text-decoration-none ms-2" href="#prices">Prices</a>
                 </Col>
-                <Col className="text-center my-auto" xs={2}>
+                <Col className="text-center my-auto  hidden-md-down" xs={12} md={2}>
                     <FontAwesomeIcon icon={faCircle} size="2xs"/>
                     <a className="text-white text-decoration-none ms-2" href="#archive">Archive</a>
                 </Col>
-                <Col className="text-center" xs={4}>
+                <Col className="text-center" xs={12} md={4}>
                     <a href="/">
                         <img
                             src="/imgs/navbar-icon.svg"
@@ -26,16 +26,24 @@ function Footer() {
                     </a>
                     <div className="align-middle text-white fs-6"><span className='text-customGreen'>Free</span> <span className='text-customRed'>API</span> for iranian Rial rates</div>
                 </Col>
-                <Col className="text-center my-auto" xs={2}>
+                <Col className="text-center text-xs-left my-auto hidden-lg-up py-3 py-md-auto" xs={6} md={2}>
+                    <FontAwesomeIcon icon={faCircle} size="2xs"/>
+                    <a className="text-white text-decoration-none ms-2" href="#prices">Prices</a>
+                </Col>
+                <Col className="text-center my-auto hidden-lg-up py-3 py-md-auto" xs={6} md={2}>
+                    <FontAwesomeIcon icon={faCircle} size="2xs"/>
+                    <a className="text-white text-decoration-none ms-2" href="#archive">Archive</a>
+                </Col>
+                <Col className="text-center my-auto py-3 py-md-auto" xs={6} md={2}>
                     <FontAwesomeIcon icon={faCircle} size="2xs"/>
                     <a className="text-white text-decoration-none ms-2" href="#graph">Graph</a>
                 </Col>
-                <Col className="text-center my-auto" xs={2}>
+                <Col className="text-center my-auto py-3 py-md-auto" xs={6} md={2}>
                     <FontAwesomeIcon icon={faCircle} size="2xs"/>
                     <a className="text-white text-decoration-none ms-2" href="#contact-us">Contact us</a>
                 </Col>
             </Row>
-            <div className="text-center footer-sentence mt-2">&copy; 2023 Bonbaz.com all rights reserved.</div>
+            <div className="text-center footer-sentence my-2">&copy; 2023 Bonbaz.com all rights reserved.</div>
         </div>
     );
 }
